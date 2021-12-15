bb7455404ed04f56060308796cc05fcc939b6af9
User 14 <User 14@PC15>
2021-12-15 08:47:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ThereDigitNumberDisection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int u, t, h;
+            int nums = int.Parse(Console.ReadLine());
+
+            if (nums >= 100 && nums <= 999)
+            {
+                u = nums % 10;
+                t = (nums / 10) % 10;
+                h = (nums / 100) % 10;
+                Console.WriteLine("Стотици = " + h);
+                Console.WriteLine("Десетици = " + t);
+                Console.WriteLine("Единици = " + u);
+            }
+            else Console.WriteLine("Числото не е трицифрено!");
+            
+        }
+       
+    }
+}
